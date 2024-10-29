8536736667adaaeff0bba75ceedd3752ab023af0
jenna.nicodemus <jenna.nicodemus@cs-kraken.centre.edu>
2024-10-29 09:26:50 -0400

    draft of priorityqueue implementation

--- src/main/java/PriorityQueue.java
+++ src/main/java/PriorityQueue.java
@@ -14,7 +14,36 @@ public class PriorityQueue<T> extends Queue<T>
     //@Override
    public void push(T val)
    {
-       super.push(val); //right now this is just a normal Queue as it will do what its parent did.
+	   Node<T> myNode = new Node<T>(val, null);
+	   if (front == null)
+	   {
+		   front = myNode;
+		   back = myNode;
+	   } else 
+	   {
+		 //  back.next = myNode;
+		 //  back = myNode;
+		   sort(val, front);
+	   }
+   }
+   
+   private void sort(T insertVal, Node<T> node) 
+   {
+	   if (node.next == null)
+	   {
+		   Node<T> myNode = new Node<T>(insertVal, null);
+		   back.next = myNode;
+		   back = myNode;
+		   return;
+	   }
+//	   if (insertVal < node.val && insertVal > node.next.val)
+	   if (compare.compare(insertVal, (T) node.val) < 0 && compare.compare(insertVal, (T) node.next.val) > 0)
+	   {
+		   Node<T> myNode = new Node<T>(insertVal, node.next);
+		   node.next = myNode;
+		   return;
+	   }
+	   sort(insertVal, node.next);
    }
 
 
